cf8fbb0fc0567036a7f387915e0c07c3b90b6602
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2024-05-09 17:13:46 +0200

    chore: make virtual_machine into a directory module

---
+++ src/virtual_machine/builtins.rs
@@ -0,0 +1,38 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use crate::parser::Expression;
+
+pub fn print(expression: Expression, end: &str) -> Expression {
+    match &expression {
+        Expression::Char(val) => print!("{}{}", *val as char, end),
+        Expression::Number(val) => print!("{}{}", val, end),
+        Expression::Bool(val) => print!("{}{}", val, end),
+        Expression::Array(type_, val) => {
+            let is_string = type_ == &crate::parser::Type::Char;
+            if !is_string {
+                print!("[");
+            }
+            for (i, item) in val.iter().enumerate() {
+                if i != 0 && !is_string {
+                    print!(", ");
+                }
+                print(item.clone(), "");
+            }
+            if !is_string {
+                print!("]");
+            }
+            print!("{}", end);
+        }
+        _ => print!("{:?}", expression),
+    }
+    return expression;
+}
+
+pub fn time() -> Expression {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let in_ms = since_the_epoch.as_millis();
+    return Expression::Number(in_ms as f64);
+}
--- src/virtual_machine/mod.rs
+++ src/virtual_machine/mod.rs
@@ -1,43 +1,10 @@
-use std::mem::discriminant;
-use std::time::{SystemTime, UNIX_EPOCH};
+mod builtins;
 
+use std::mem::discriminant;
 use crate::parser::{BinaryOperator, Declaration, Expression, Item};
 use crate::scoped_symbols::ScopedSymbols;
 
-fn print(expression: Expression, end: &str) -> Expression {
-    match &expression {
-        Expression::Char(val) => print!("{}{}", *val as char, end),
-        Expression::Number(val) => print!("{}{}", val, end),
-        Expression::Bool(val) => print!("{}{}", val, end),
-        Expression::Array(type_, val) => {
-            let is_string = type_ == &crate::parser::Type::Char;
-            if !is_string {
-                print!("[");
-            }
-            for (i, item) in val.iter().enumerate() {
-                if i != 0 && !is_string {
-                    print!(", ");
-                }
-                print(item.clone(), "");
-            }
-            if !is_string {
-                print!("]");
-            }
-            print!("{}", end);
-        }
-        _ => print!("{:?}", expression),
-    }
-    return expression;
-}
-
-fn time() -> Expression {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    let in_ms = since_the_epoch.as_millis();
-    return Expression::Number(in_ms as f64);
-}
+use self::builtins::{time, print};
 
 fn binary_float_float(left_val: f64, right_val: f64, operator: BinaryOperator) -> Expression {
     match operator {
